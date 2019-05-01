e29a69d2bdc4a70ef1694dd78eef945f7f9ccdb5
wguo1877 <billy.r.guo@gmail.com>
2019-05-01 02:54:10 -0500

    finished results and analysis on paper

--- src/data_viz.R
+++ src/data_viz.R
@@ -110,17 +110,8 @@ ggplot(data_without, aes(x=tsne_new_without$Y[,1], y=tsne_new_without$Y[,2], col
   ggtitle("t-SNE visualization of our variables of interest") + theme(plot.title = element_text(hjust = 0.5)) + 
   xlab("Dimension 1") + ylab("Dimension 2")
 
-### PCA ###
-# no preprocessing
-pca <- prcomp(data_without) 
-autoplot(pca, data=data, colour="V144")
-
-# all ordinal variables converted to dummies
-pca_dummy <- prcomp(dummy_data_without) 
-autoplot(pca_dummy, data=dummy_data, colour="V144")
-
 ### MCA ###
-x <- as.data.frame(data_without)
+x <- as.data.frame(data_without[c(1:2, 5:8, 10:13)])
 i=0
 while(i < ncol(x)){
   i=i+1  
@@ -134,8 +125,8 @@ fviz_mca_var(mca_res, choice = "mca.cor",
              repel = TRUE, # Avoid text overlapping (slow)
              ggtheme = theme_minimal())
 
-# inner feelings regression
-model1 <- lm(ind$coord[, 1] ~ V144 + V242 + V240 + V248, data=data)
+# outer feelings regression
+model1 <- lm(ind$coord[, 1] ~ + V242 + V240 + V248 + V96 + V98 + V99, data=data)
 
 # V144: Religious denomination
 # V242: Age
@@ -149,5 +140,5 @@ model1 <- lm(ind$coord[, 1] ~ V144 + V242 + V240 + V248, data=data)
 model2 <- ivreg(ind$coord[, 2] ~ V144 +  V96 + V98 + V99 | .-V96 + V239, data=data)
 
 # summary
-stargazer(model1, model2, type="latex")
+stargazer(model1, type="latex")
 
